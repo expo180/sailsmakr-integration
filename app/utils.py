032847b2c4c30dfb9d557aca737eb4241af73ffe
expo180 <badersaadousalissou@gmail.com>
2024-06-14 15:12:34 +0100

    15:12

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,10 @@
 def get_tasks_for_user(user_email):
-    from .models import Task
-    tasks = Task.query.filter_by(assigned_to=user_email).all()
+    from .models import Task, User
+
+    user = User.query.filter_by(email=user_email).first()
+    if user:
+        tasks = Task.query.filter_by(assigned_to=user.id).all()
+    else:
+        tasks = []
 
     return tasks
